939cc395fec558ed8d5b3ed33bb949baddce1440
Thino <rakotomalalathino@gmail.com>
2023-12-30 23:12:35 +0300

    Debugging

--- src/components/Productivity/Productivity.tsx
+++ src/components/Productivity/Productivity.tsx
@@ -1,14 +1,19 @@
 import config from '@/utils/config';
 import './Productivity.scss';
 import { GoBriefcase } from 'react-icons/go';
+import { motion } from 'framer-motion';
 const Productivity = () => {
     return (
         <div className="Productivity">
             <div className="Productivity-main">
                 <div className="Productivity-shape">
-                    <div className="go-briefcase-container">
+                    <motion.div
+                        initial={{ opacity: 0 }}
+                        whileInView={{ opacity: 1 }}
+                        className="go-briefcase-container"
+                    >
                         <GoBriefcase color="#fff" className="code-icon" />
-                    </div>
+                    </motion.div>
                     <div className="line-shape-2"></div>
                 </div>
                 <div className="Productivity-content">
